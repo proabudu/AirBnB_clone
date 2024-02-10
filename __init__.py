23d7a98b3deb6cfc70f5139b4b9ad31656e812a3
PROSPER ABUDU <128247250+proabudu@users.noreply.github.com>
2024-02-10 11:47:20 +0000

    a

--- __init__.py
+++ __init__.py
@@ -1,1 +1,7 @@
-# intentionally left empty
+from models.engine.file_storage import FileStorage
+
+# Create a storage instance
+storage = FileStorage()
+
+# Load data from the JSON file into the storage instance
+storage.reload()
