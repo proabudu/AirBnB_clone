7db6a28704cfd42c705f879351fecde5c84bb885
PROSPER ABUDU <128247250+proabudu@users.noreply.github.com>
2024-02-10 16:44:27 +0000

    quck changes to project

--- __init__.py
+++
@@ -1,7 +0,0 @@
-from models.engine.file_storage import FileStorage
-
-# Create a storage instance
-storage = FileStorage()
-
-# Load data from the JSON file into the storage instance
-storage.reload()
