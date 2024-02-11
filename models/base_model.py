6b326b29e98fc82c03b436b098a130dccafe5d5a
PROSPER ABUDU <128247250+proabudu@users.noreply.github.com>
2024-02-11 08:42:21 +0000

    base model update

--- models/base_model.py
+++ models/base_model.py
@@ -5,17 +5,20 @@ from datetime import datetime
 
 
 class BaseModel:
-    """Base class for all application models  attributes and methods."""
+    """Base class for all models with common attributes and methods."""
 
     def __init__(self):
-        """Initializes a new BaseModel instance with, and other attributes."""
+        """Initializes with unique ID, timestamps, and other attributes."""
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = self.created_at
 
     def __str__(self):
         """Returns a human-readable string rep of the model instance."""
-        return f"[<class name>: {type(self).__name__}] (<ID>: {self.id}) {self.__dict__}"
+        return (
+            f"[<class name>: {type(self).__name__}] "
+            f"(<ID>: {self.id}) {self.__dict__}"
+        )  # Line 18 split into multiple lines
 
     def save(self):
         """Updates the 'updated_at' timestamp to reflect latest changes."""
@@ -38,4 +41,4 @@ print(my_model)
 if my_model.__class__.__name__ == "BaseModel":
     print("It's a base model with no additional attributes!")
 else:
-    print("It's aextended model with custom attributes:", my_model.__dict__)
+    print("It's  extended model with custom attributes:", my_model.__dict__)
