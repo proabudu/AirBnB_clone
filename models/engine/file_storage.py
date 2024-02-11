225c2c884d4e5b97771f75a71eba99b8f6ce942f
PROSPER ABUDU <128247250+proabudu@users.noreply.github.com>
2024-02-11 17:14:26 +0000

    quick changes

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -2,6 +2,7 @@
 
 import json
 
+
 class FileStorage:
     __file_path = "file.json"
     __objects = {}
@@ -17,7 +18,7 @@ class FileStorage:
         serialized_objects = {}
         for key, obj in FileStorage.__objects.items():
             serialized_objects[key] = obj.to_dict()
-        
+
         with open(FileStorage.__file_path, 'w') as file:
             json.dump(serialized_objects, file)
 
