6f23e03e6a3e81825c8d82f1d1db15ff906ac250
PROSPER ABUDU <128247250+proabudu@users.noreply.github.com>
2024-02-11 11:33:05 +0000

    upload fists file for tests/models_tests/

---
+++ tests/models_tests/test_base_model.py
@@ -0,0 +1,52 @@
+# content of tests/test_base_model.py
+import unittest
+from models.base_model import BaseModel
+
+
+class TestBaseModel(unittest.TestCase):
+    """This class tests the BaseModel class"""
+
+    def setUp(self):
+        """This method creates an instance of BaseModel for each test"""
+        self.user = BaseModel()
+
+    def test_init(self):
+        """This method tests the initialization of the instance attributes"""
+        self.assertEqual(self.user.profile["active"], False)
+        self.assertEqual(self.user.profile["level"], 1)
+        self.assertEqual(self.user.profile["points"], 0)
+
+    def test_activate(self):
+        """This method tests the activate method"""
+        self.user.activate()
+        self.assertEqual(self.user.profile["active"], True)
+
+    def test_is_active(self):
+        """This method tests the is_active method"""
+        self.assertFalse(self.user.is_active())
+        self.user.activate()
+        self.assertTrue(self.user.is_active())
+
+    def test_get_level(self):
+        """This method tests the get_level method"""
+        self.assertEqual(self.user.get_level(), 1)
+        self.user.add_points(250)
+        self.assertEqual(self.user.get_level(), 2)
+        self.user.add_points(100)
+        self.assertEqual(self.user.get_level(), 3)
+
+    def test_get_points(self):
+        """This method tests the get_points method"""
+        self.assertEqual(self.user.get_points(), 0)
+        self.user.add_points(50)
+        self.assertEqual(self.user.get_points(), 50)
+
+    def test_add_points(self):
+        """This method tests the add_points method"""
+        self.user.add_points(100)
+        self.assertEqual(self.user.profile["points"], 100)
+        self.user.add_points(-50)
+        self.assertEqual(self.user.profile["points"], 50)
+
+if __name__ == "__main__":
+    unittest.main()
