3e97ab320cf2817dbd8f5e05730cdcaf32209ece
PROSPER ABUDU <128247250+proabudu@users.noreply.github.com>
2024-02-11 22:50:12 +0000

    as

--- models/base_model.py
+++ models/base_model.py
@@ -1,19 +1,25 @@
 #!/usr/bin/python3
 
-"""BaseModel class Module is defined here."""
+"""
+BaseModel class Module is defined here.
+"""
 
 import uuid
 import datetime
 
 
 class BaseModel:
-    """This class defines common attributes and methods for other classes."""
+    """
+    This class defines common attributes and methods for other classes.
+    """
 
     def __init__(self, *args, **kwargs):
-        """Initialize instance attributes."""
+        """
+        Initialize instance attributes.
+        """
         if kwargs:
             for key, value in kwargs.items():
-                if key in ['created_at', 'updated_at']:
+                if key == 'created_at' or key == 'updated_at':
                     value = datetime.datetime.strptime(
                         value, "%Y-%m-%dT%H:%M:%S.%f")
                 if key != "__class__":
@@ -28,20 +34,24 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.datetime.now()
             self.updated_at = datetime.datetime.now()
-            storage.new(self)
 
     def __str__(self):
-        """Return a string representation of the object."""
+        """
+        Return a string representation of the object.
+        """
         return "[{}] ({}) {}".format(
             self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
-        """Update the updated_at attribute with current datetime."""
+        """
+        Update the updated_at attribute with current datetime.
+        """
         self.updated_at = datetime.datetime.now()
-        storage.save()
 
     def to_dict(self):
-        """Return a dictionary representation of the object."""
+        """
+        Return a dictionary representation of the object.
+        """
         obj_dict = self.__dict__.copy()
         obj_dict["__class__"] = self.__class__.__name__
         obj_dict["created_at"] = self.created_at.isoformat()
