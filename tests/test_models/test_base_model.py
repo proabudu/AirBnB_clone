69bb2d063c8d6173858e08fafc4cc074ccf33e6e
PROSPER ABUDU <128247250+proabudu@users.noreply.github.com>
2024-03-10 02:59:06 +0000

    adding documentation to test_base_model.py file

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -3,21 +3,25 @@
 import unittest
 from models.base_model import BaseModel
 
+
 class TestBaseModel(unittest.TestCase):
 
     def test_attributes(self):
+        # Test if BaseModel attributes are present
         model = BaseModel()
         self.assertTrue(hasattr(model, 'id'))
         self.assertTrue(hasattr(model, 'created_at'))
         self.assertTrue(hasattr(model, 'updated_at'))
 
     def test_save(self):
+        # Test if BaseModel save method updates 'updated_at'
         model = BaseModel()
         original_updated_at = model.updated_at
         model.save()
         self.assertNotEqual(original_updated_at, model.updated_at)
 
     def test_to_dict(self):
+        # Test if BaseModel to_dict method returns the expected dictionary
         model = BaseModel()
         model_dict = model.to_dict()
         self.assertEqual(model_dict['__class__'], 'BaseModel')
@@ -26,6 +30,8 @@ class TestBaseModel(unittest.TestCase):
         self.assertIn('updated_at', model_dict)
 
     def test_str(self):
+        # Test if BaseModel __str__ method returns
+        # the expected string representation
         model = BaseModel()
         string_representation = str(model)
         self.assertIn('BaseModel', string_representation)
