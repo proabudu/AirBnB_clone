9025283f0dc5068ed401405571910e908fe256ed
PROSPER ABUDU <128247250+proabudu@users.noreply.github.com>
2024-03-10 03:25:23 +0000

    again

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -5,32 +5,35 @@ from models.base_model import BaseModel
 
 
 class FileStorage:
-    __file_path = "file.json"
-    __objects = {}
+    """Provides persistent storage for objects using a JSON file."""
+
+    __file_path = "file.json"  # Path to the storage file
+    __objects = {}  # In-memory cache of stored objects
 
     def all(self):
+        """Returns a dictionary of all stored objects."""
         return FileStorage.__objects
 
     def new(self, obj):
+        """Adds a new object to the storage."""
         key = "{}.{}".format(obj.__class__.__name__, obj.id)
         FileStorage.__objects[key] = obj
 
     def save(self):
-        serialized_objects = {}
-        for key, obj in FileStorage.__objects.items():
-            serialized_objects[key] = obj.to_dict()
-
+        """ Saves all objects to the JSON file."""
         with open(FileStorage.__file_path, 'w') as file:
-            json.dump(serialized_objects, file)
+            json.dump({key: obj.to_dict()  # Break after to_dict()
+                   for key, obj in FileStorage.__objects.items()}, file)
 
     def reload(self):
+        """Reloads objects from the JSON file,the file doesn't exist."""
         try:
             with open(FileStorage.__file_path, 'r') as file:
                 data = json.load(file)
                 for key, value in data.items():
                     class_name, obj_id = key.split('.')
-                    cls = eval(class_name)
-                    obj = cls(**value)
+                    cls = eval(class_name)  # Recreate class dynamically
+                    obj = cls(**value)  # Instantiate object from dictionary
                     FileStorage.__objects[key] = obj
         except FileNotFoundError:
             pass
