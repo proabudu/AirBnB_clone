6a3500c16796edbb68411851b9e88eb78f78406d
PROSPER ABUDU <128247250+proabudu@users.noreply.github.com>
2024-03-10 03:09:51 +0000

    comment in test

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -7,21 +7,21 @@ from models.base_model import BaseModel
 class TestBaseModel(unittest.TestCase):
 
     def test_attributes(self):
-        # Test if BaseModel attributes are present
+        """ Test if BaseModel attributes are present """
         model = BaseModel()
         self.assertTrue(hasattr(model, 'id'))
         self.assertTrue(hasattr(model, 'created_at'))
         self.assertTrue(hasattr(model, 'updated_at'))
 
     def test_save(self):
-        # Test if BaseModel save method updates 'updated_at'
+        """ Test if BaseModel save method updates 'updated_at' """
         model = BaseModel()
         original_updated_at = model.updated_at
         model.save()
         self.assertNotEqual(original_updated_at, model.updated_at)
 
     def test_to_dict(self):
-        # Test if BaseModel to_dict method returns the expected dictionary
+        """ Test if BaseModel to_dict method returns expected dictionary """
         model = BaseModel()
         model_dict = model.to_dict()
         self.assertEqual(model_dict['__class__'], 'BaseModel')
@@ -30,8 +30,7 @@ class TestBaseModel(unittest.TestCase):
         self.assertIn('updated_at', model_dict)
 
     def test_str(self):
-        # Test if BaseModel __str__ method returns
-        # the expected string representation
+        """ Test if BaseModel __str__ method returns """
         model = BaseModel()
         string_representation = str(model)
         self.assertIn('BaseModel', string_representation)
